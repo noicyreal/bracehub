67e24c41e4f5dd9e5d5a568ad7d848dd8b35d82f
noicyreal <noic@riseup.net>
2026-09-10 19:52:40 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,9 +6,9 @@ local scripts = {
     [17738127017] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/brace.lua",
     [83443937211921] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/guess.lua",
     [139988436996662] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/stopthetimer.lua",
-    [574407221] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/superherotycoon.lua"
-    [132640332499066] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/anarchy.luau"
-    [5938036553] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/frontlines.luau"
+    [574407221] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/superherotycoon.lua",
+    [132640332499066] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/anarchy.luau",
+    [5938036553] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/frontlines.luau",
     [110808833601416] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/cham.lua"
 }
 
@@ -18,6 +18,10 @@ if url then
     local success, err = pcall(function()
         loadstring(game:HttpGet(url))()
     end)
+
+    if not success then
+        warn("Failed to load script:", err)
+    end
 else
     player:Kick("Game not supported.")
 end
