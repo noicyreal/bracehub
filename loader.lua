b4c72105fffaf507bf9f7b75e91ba547bb97531b
noicyreal <noic@riseup.net>
2026-09-10 19:56:17 +0200

    Update script ID for guess.lua in loader.lua

--- loader.lua
+++ loader.lua
@@ -4,7 +4,7 @@ local placeId = game.PlaceId
 
 local scripts = {
     [17738127017] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/brace.lua",
-    [83443937211921] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/guess.lua",
+    [129870876180628] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/guess.lua",
     [139988436996662] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/stopthetimer.lua",
     [574407221] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/superherotycoon.lua",
     [132640332499066] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/anarchy.luau",
