5a45ca5ae47d925249f55e85601e65fc2a5a590b
noicyreal <noic@riseup.net>
2026-09-10 17:41:31 +0200

    v2

--- loader.lua
+++ loader.lua
@@ -7,6 +7,9 @@ local scripts = {
     [83443937211921] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/guess.lua",
     [139988436996662] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/stopthetimer.lua",
     [574407221] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/superherotycoon.lua"
+    [132640332499066] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/anarchy.luau"
+    [5938036553] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/frontlines.luau"
+    [110808833601416] = "https://raw.githubusercontent.com/noicyreal/bracehub/main/cham.lua"
 }
 
 local url = scripts[placeId]
